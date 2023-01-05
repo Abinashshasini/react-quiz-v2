a908e7de563c693832876819e2a0faa61caa16b0
abinash.shasini <abinashshasini125@gmail.com>
2023-01-05 13:17:28 +0530

    Third commit

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
-import React, { FC } from 'react';
+import { FC } from 'react';
 
 interface Props {
   score: number;
@@ -8,38 +8,16 @@ interface Props {
 const GameOver: FC<Props> = ({ score }) => {
   return (
     <div>
-      {(() => {
-        if (score > 5) {
-          return (
-            <div>
-              <iframe
-                src="https://giphy.com/embed/DwRN1GYnF7TqTUEeei"
-                width="480"
-                height="480"
-                frameBorder="0"
-                className="giphy-embed"
-                allowFullScreen
-              ></iframe>
-              <p>Noiceee Baklol...</p>
-            </div>
-          );
-        } else {
-          return (
-            <div>
-              <iframe
-                src="https://giphy.com/embed/DwRN1GYnF7TqTUEeei"
-                width="480"
-                height="480"
-                frameBorder="0"
-                className="giphy-embed"
-                allowFullScreen
-              ></iframe>
-
-              <p>Bhak betichod...</p>
-            </div>
-          );
-        }
-      })()}
+      <iframe
+        src="https://giphy.com/embed/DwRN1GYnF7TqTUEeei"
+        width="480"
+        height="480"
+        frameBorder="0"
+        className="giphy-embed"
+        allowFullScreen
+      ></iframe>
+      <div>Congratulation!</div>
+      <div>Your Scored {score}</div>
     </div>
   );
 };
